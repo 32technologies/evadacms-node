14bbd94eb20e9c5637caee1f434486be4908c58c
Eric Zimmerman <eric@32technologies.com>
2018-02-07 14:50:45 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "semi": 0,
+    "linebreak-style": 0,
+    "space-before-function-paren": 0,
+    "comma-dangle": 0,
+    "class-methods-use-this": 0,
+    "no-param-reassign": 0
+  }
+};
